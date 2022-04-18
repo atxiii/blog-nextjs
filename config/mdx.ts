8c2b51717d1087dc36f84fe9b646d1d7ca344c9f
Hosein Shurabi <MRCAT@Mrcat-Mac.local>
2022-04-18 22:03:59 +0430

    remove unused imports

--- config/mdx.ts
+++ config/mdx.ts
@@ -1,13 +1,4 @@
-import { Pluggable } from 'unified';
-import { remarkCodeHike } from '@code-hike/mdx';
-import rehypeCodeTitles from 'rehype-code-titles';
 import rehypePrism from 'rehype-prism-plus';
-import rehypeHighlight from 'rehype-highlight';
-import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
-import remarkParse from 'remark-parse';
-
-import theme from '../code-hike-theme';
-
 import remarkGfm from 'remark-gfm';
 
 const mdxOptions = {
